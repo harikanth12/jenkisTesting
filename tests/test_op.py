2127adef99a259b670359de1e11edf2f5dfe8bb7
unknown <harikanth12>
2025-02-09 18:02:49 +0530

    override the required function names to respective test cases

--- tests/test_op.py
+++ tests/test_op.py
@@ -4,13 +4,13 @@ def test_add():
     assert res == 7
 
 def test_sub():
-    res = add(3, 4)
+    res = sub(3, 4)
     assert res == -1
 
 def test_multply():
-    res = add(3, 4)
+    res = multply(3, 4)
     assert res == 12
 
 def test_division():
-    res = add(2, 4)
+    res = division(2, 4)
     assert res == 0.5
