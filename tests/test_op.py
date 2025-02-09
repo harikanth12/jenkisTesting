202ff984cd3ad0d574f23b62843bf69a8c6d9999
unknown <harikanth12>
2025-02-09 18:01:22 +0530

    removed function parameters

--- tests/test_op.py
+++ tests/test_op.py
@@ -3,14 +3,14 @@ def test_add():
     res = add(3,4)
     assert res == 7
 
-def test_sub(x,y):
+def test_sub():
     res = add(3, 4)
     assert res == -1
 
-def test_multply(x,y):
+def test_multply():
     res = add(3, 4)
     assert res == 12
 
-def test_division(x,y):
+def test_division():
     res = add(2, 4)
     assert res == 0.5
