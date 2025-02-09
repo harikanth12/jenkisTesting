0eb4a3aed13a69d7e22b739255071e22508983ff
unknown <harikanth12>
2025-02-09 17:33:36 +0530

    testing files are added

---
+++ ops.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def multply(x,y):
+    return x*y
+
+def division(x,y):
+    return x/y
---
+++ tests/test_op.py
@@ -0,0 +1,16 @@
+from ops import *
+def test_add():
+    res = add(3,4)
+    assert res == 7
+
+def test_sub(x,y):
+    res = add(3, 4)
+    assert res == -1
+
+def test_multply(x,y):
+    res = add(3, 4)
+    assert res == 12
+
+def test_division(x,y):
+    res = add(2, 4)
+    assert res == 0.5
